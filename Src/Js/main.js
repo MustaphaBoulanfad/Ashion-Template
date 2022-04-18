87bdd359a427311992e6ec22f42df559dc2f477a
MustaphaBoulanfad <Boulanfad.M01@gmail.com>
2022-04-18 12:31:45 +0000

    Create the Products section

--- Src/Js/main.js
+++ Src/Js/main.js
@@ -1,23 +1,27 @@
 import "../Css/Sass/main.scss";
 import "../Css/Vendors/normalize.css";
-import imageOne from "../Images/header-image-01.webp";
-import imageTwo from "../Images/header-image-02.webp";
-import imageThree from "../Images/header-image-03.webp";
-import imageFour from "../Images/header-image-04.webp";
-import imageFive from "../Images/header-image-05.webp";
+import "../Images/header-image-01.webp";
+import "../Images/header-image-02.webp";
+import "../Images/header-image-03.webp";
+import "../Images/header-image-04.webp";
+import "../Images/header-image-05.webp";
+import "../Images/product-1.jpg";
+import "../Images/product-2.jpg";
+import "../Images/product-3.jpg";
+import "../Images/product-4.jpg";
+import "../Images/product-5.jpg";
+import "../Images/product-6.jpg";
+import "../Images/product-7.jpg";
+import "../Images/product-8.jpg";
 
 const btnNavMobile = document.querySelector(".nav-mobile"),
   navbar_moible = document.querySelector(".nav"),
   dropdown_links = document.querySelectorAll(".dropdown"),
   descriptions = Array.from(
     document.querySelectorAll(".desc-container .description")
-  );
-
-for (let i = 0; i < descriptions.length; i++) {
-  descriptions[i].style.backgroundImage = `url("./Images/header-image-0${
-    i + 1
-  }.webp")`;
-}
+  ),
+  products_images = Array.from(document.querySelectorAll(".product .image")),
+  showProductsSpan = Array.from(document.querySelectorAll(".show-product"));
 
 // Check the width of window
 function checkWidth() {
@@ -59,4 +63,44 @@ document.addEventListener("click", () => {
   ) {
     navbar_moible.classList.remove("show");
   }
+});
+
+// Add the background Images to the descriptions
+for (let i = 0; i < descriptions.length; i++) {
+  descriptions[i].style.backgroundImage = `url("./Images/header-image-0${
+    i + 1
+  }.webp")`;
+}
+
+// Add the Background images to products
+for (let i = 0; i < products_images.length; i++) {
+  products_images[i].style.backgroundImage = `url("./Images/product-${
+    i + 1
+  }.jpg")`;
+}
+
+// Show the image when click on the span
+showProductsSpan.forEach((show) => {
+  let showImageContainer = document.createElement("div");
+  let image = document.createElement("img");
+
+  show.addEventListener("click", () => {
+    let number = show.dataset.number;
+    showImageContainer.classList.add("show-product-image");
+    image.setAttribute("src", `./Images/product-${number}.jpg`);
+
+    showImageContainer.appendChild(image);
+    document.body.prepend(showImageContainer);
+    document.body.classList.add("open");
+  });
+  // Remove the modal when i clicked out of image
+  showImageContainer.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "img".toUpperCase()) {
+      image.remove();
+      showImageContainer.remove();
+      document.body.classList.remove("open");
+    } else {
+      event.preventDefault();
+    }
+  });
 });
